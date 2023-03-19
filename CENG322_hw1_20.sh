180e769f20893de13bf55e6083806c077e5bdf32
Mert98 <102909932+Mert9898@users.noreply.github.com>
2023-03-19 18:11:26 +0300

    First
    1

---
+++ CENG322_hw1_20.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#250201017 Yağmur Nisa Şekerci
+#260201011 Mert Ayaz Özkan
+
+read -p "Enter the name of the directory: " Directory
+read -p "Enter the keyword: " Keyword
+
+function copy(){
+
+
+
+}
+
+function display_details(){
+
+
+
+}
