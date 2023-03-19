3f6b839faa7d1fcfb38d79d1325955dbc6e797c1
Mert98 <102909932+Mert9898@users.noreply.github.com>
2023-03-19 19:53:00 +0300

    Update CENG322_hw1_20.sh

--- CENG322_hw1_20.sh
+++ CENG322_hw1_20.sh
@@ -6,8 +6,21 @@ read -p "Enter the name of the directory: " Directory
 read -p "Enter the keyword: " Keyword
 
 function copy(){
+	
+	mkdir /Found
 
+	for _fl_ in "$Directory";do
+		
+	if[-f _f1_ && grep -r $Keyword _f1_];
 
+		then
+			cp $_f1_ "$Directory/Found"
+			mv "$_f1_" "Found_ $_f1_"
+			echo ("Files were copied to the Found directory!")
+		else
+			echo("Keyword not found in files!")
+
+      fi
 
 }
 
