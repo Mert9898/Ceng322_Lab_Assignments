a9a10c306de40e79f425af3f442c2d1264095bc3
Mert98 <102909932+Mert9898@users.noreply.github.com>
2023-03-21 21:25:02 +0000

    Update CENG322_hw1_20.sh

--- CENG322_hw1_20.sh
+++ CENG322_hw1_20.sh
@@ -2,27 +2,44 @@
 #250201017 Yağmur Nisa Şekerci
 #260201011 Mert Ayaz Özkan
 
+# Get the search directory from the user
 read -p "Enter the name of the directory: " Directory
-read -p "Enter the keyword: " Keyword
+
+# Get the search keyword from the user
+read -p "Enter the search keyword: " Keyword
+
+
 
 function copy(){
-	
-	mkdir /Found
 
-	for _fl_ in "$Directory";do
-		
-	if[-f _f1_ && grep -r $Keyword _f1_];
+	# Create the Found directory if it doesn't already exist
+	if [ ! -d Found ]; then
+    	mkdir Found
+	fi
 
-		then
-			cp $_f1_ "$Directory/Found"
-			mv "$_f1_" "Found_ $_f1_"
-			echo ("Files were copied to the Found directory!")
+	# Loop through all the files in the search directory
+	for file in "$Directory"/*
+		do
+    		# Check if the file contains the search keyword
+    		if grep -q "$Keyword" "$file"; then
+        		# Copy the file to the Found directory
+        		cp "$file" Found/
+        		echo "File '$file' was copied to the Found 			directory."
+    		fi
+		done
+
+		# Check if any files were copied to the Found directory
+		if [ "$(ls -A Found)" ]; then
+    			echo "The following files were found:"
+    			ls Found/
 		else
-			echo("Keyword not found in files!")
-
-      fi
-
+    			echo "Keyword not found in files."
+		fi
 }
+
+
+
+copy
 
 function display_details(){
 
