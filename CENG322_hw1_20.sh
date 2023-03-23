740dd96fe18743574d315b1f8c9492d89d8d8c58
yagmurnisa <ymr.nisa@hotmail.com>
2023-03-23 22:20:43 +0300

    updated

--- CENG322_hw1_20.sh
+++ CENG322_hw1_20.sh
@@ -8,48 +8,43 @@ read directory
 echo "Enter the keyword:"
 read keyword
 
-function copy(){
-
-	# Create the Found directory if it doesn't already exist
-	if [ ! -d "Found" ]; then
-  		mkdir Found
-	fi
-
-	# Search for files containing keyword
-	found=false
-
-	for file in "${directory}/"*; 
-	do
-  		if grep -q "${keyword}" "${file}"; then
-    			found=true
-    			 filename=$(basename "${file}")
-    			newname="Found_${filename}"
-    			# Copy file to Found directory with new name
-    			cp "${file}" "Found/${newname}"
-  		fi
-	done
-
-	if [ $found == true ]; then
-  		echo "Files copied to the Found directory!"
-  		echo "Files in the Found directory:"
-  		ls Found/
-	else
-  		echo "Keyword not found in files."
-	fi
+copy () {
+    FOUND_FILES=$(grep -rl $keyword $directory)
+    if [ "$FOUND_FILES" ]
+        then
+            if [ ! -d "Found" ]
+				then
+  					mkdir Found
+			fi
+			# Create the Found directory if it doesn't already exist
+            for file in $FOUND_FILES
+                do
+					filename=$(basename "${file}")
+					newname="Found_${filename}"
+					# Copy file to Found directory with new name
+					cp "${file}" "Found/${newname}"
+                done
+            echo "Files were copied to the Found directory!"
+            ls Found
+        else
+            echo "Keyword not found in files!"
+    fi
 }
 
 copy
 
-
-function modification_details(){
-	for file in "Found/"*; 
-	
-		do
-  		modification=$(stat -c "File %n: %F was modified by %U on %y" "${file}")
-		echo "${modification}"
-  		echo "${modification}" >> Found/modification_details.txt
-		done
-
+modification_details(){
+	num=0
+	if [ -d "Found" ]
+		then
+			for file in "Found/"
+				do
+					modification=$(stat -c "File $num: %n was modified by %U on %y" "${file}")
+					echo "${modification}"
+					echo "${modification}" >> Found/modification_details.txt
+					num=$((num+1))
+				done
+	fi
 }
 
 modification_details
